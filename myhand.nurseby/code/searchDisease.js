42afe1eec45bd3753d5550b3fb36484eee3a51c2
lifaon <sm0514sm@gmail.com>
2019-10-16 01:10:55 +0900

    2. 코어 증상으로 검색하는 js 만들기 (searchDisease.js 만 완료)

--- myhand.nurseby/code/searchDisease.js
+++ myhand.nurseby/code/searchDisease.js
@@ -4,7 +4,7 @@ module.exports.function = function searchDisease(Part, Symptom) {
   const symptomProcess = require("./data/symptomProcess.js");
   const SCORE = require("./data/scoreInfo.js");
 
-  
+
   /*------ 선언부 ------*/
   let results = [];
 
@@ -41,9 +41,10 @@ module.exports.function = function searchDisease(Part, Symptom) {
 
   /*------ 실행 ------*/
   if (Part == null && Symptom == null) {
-    // 둘 다 못찾는 경우
+    // 둘 다 못찾는 경우 < 근데 여기에 들어오진 못할듯
   }
   else {
+    /* part 검사 */
     if (Part != null && !(Part.length == 0)) {
       Part.forEach(function (item, index, array) {
         for (let i = 0; i < diseaseData.length; i++) {
@@ -55,14 +56,26 @@ module.exports.function = function searchDisease(Part, Symptom) {
         }
       });
     }
-    if (Symptom != null) {
-      for (let i = 0; i < diseaseData.length; i++) {
-        if (diseaseData[i].symptom.includes(Symptom)) {
-          diseaseData[i].score += SCORE.symptom;
-          if (results.indexOf(diseaseData[i]) == -1)
-            results.push(diseaseData[i]);
+    /* Symptom 검사 */
+    if (Symptom != null && !(Symptom.length == 0)) {
+      Symptom.forEach(function (item, index, array) {
+        /* Core Symptom 검사 */
+        for (let i = 0; i < diseaseData.length; i++) {
+          if (diseaseData[i].core_symptom != null && diseaseData[i].core_symptom.includes(item)) {
+            diseaseData[i].score += SCORE.core_symptom;
+            if (results.indexOf(diseaseData[i]) == -1)
+              results.push(diseaseData[i]);
+          }
         }
-      }
+        /* Symptom 검사 */
+        for (let i = 0; i < diseaseData.length; i++) {
+          if (diseaseData[i].symptom.includes(item)) {
+            diseaseData[i].score += SCORE.symptom;
+            if (results.indexOf(diseaseData[i]) == -1)
+              results.push(diseaseData[i]);
+          }
+        }
+      })
     }
   }
   // score 높은 순으로 정렬
@@ -71,7 +84,7 @@ module.exports.function = function searchDisease(Part, Symptom) {
   });
 
   let newResults = [];
-  for(let i=0; i< 5 ; i++){
+  for (let i = 0; i < 10; i++) {
     newResults.push(results[i]);
   }
   console.log(results);
